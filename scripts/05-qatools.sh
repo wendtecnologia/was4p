93d2fe3dd421b7f02455fcd08ed25c9c50d6dc41
Walker de Alencar <walkeralencar@gmail.com>
2013-10-29 18:06:29 -0200

    [UPDATE] - removido os links dinâmicos, closes #9 [INFO] - na correcao do fedora e mint, fixed #11

--- scripts/05-qatools.sh
+++ scripts/05-qatools.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-echo -e -n "Instalando PHP Quality Assurance Toolchain..."
+echo -n "Instalando PHP Quality Assurance Toolchain..."
 
-/usr/local/zend/bin/pear channel-discover pear.phpqatools.org
-auto_discover=`/usr/local/zend/bin/pear config-get auto_discover`
-/usr/local/zend/bin/pear config-set auto_discover 1
-/usr/local/zend/bin/pear install phpqatools/phpqatools
-/usr/local/zend/bin/pear config-set auto_discover $auto_discover
-
-ln -s /usr/local/zend/bin/phpunit /usr/bin/phpunit
-ln -s /usr/local/zend/bin/pdepend /usr/bin/pdepend
-ln -s /usr/local/zend/bin/phpmd /usr/bin/phpmd
-ln -s /usr/local/zend/bin/phpcpd /usr/bin/phpcpd
-ln -s /usr/local/zend/bin/phploc /usr/bin/phploc
-ln -s /usr/local/zend/bin/phpcb /usr/bin/phpcb
-ln -s /usr/local/zend/bin/phpcs /usr/bin/phpcs
+pear channel-discover pear.phpqatools.org
+auto_discover=`pear config-get auto_discover`
+pear config-set auto_discover 1
+pear install phpqatools/phpqatools
+pear config-set auto_discover $auto_discover
 
 echo "Ok"
