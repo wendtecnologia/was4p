10ab7cd767afb757bc08d6ceafd0c4ca1a073354
ericovasconcelos <erico@ettbrasil.com.br>
2013-12-10 18:24:22 -0200

    Update 05-qatools.sh
    Inclusão da linha para instalar o PEAR

--- scripts/05-qatools.sh
+++ scripts/05-qatools.sh
@@ -2,6 +2,9 @@
 
 echo -n "Instalando PHP Quality Assurance Toolchain..."
 
+apt-get install php-pear
+pear channel-update pear.php.net
+pear upgrade pear
 pear channel-discover pear.phpqatools.org
 auto_discover=`pear config-get auto_discover`
 pear config-set auto_discover 1
