6e5c5f69f59f5f1e1aa608adee4c65d0bfef58c2
Walker de Alencar <walkeralencar@gmail.com>
2014-08-05 05:10:32 -0300

    [UPDATE] - Adicionada uma pausa entre a instalação e a definição dos paths para que o usuario tenha tempo de acessas a ZendServer UI, fazer as configurações iniciais e depois finalizar o processo;

--- install.sh
+++ install.sh
@@ -1,13 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 ###
 # WAS4P - Web Application Server for PHP by [Wend Tecnologia (@wendtecnologia)]
 # Author: Walker de Alencar (@walkeralencar)
 # 
 ###
 
+## Functions
+function pause(){
+   read -p "$*"
+}
+function pausePressKey(){ 
+   pause "Press [Enter] key to continue..."
+}
+
 ## Constants
 root_path=`dirname $0`
 
 cd $root_path/scripts
+
 ./04-zendserver.sh
+
+echo ""
+echo -n "ATENTION: Access Zend Server UI, complete all steps. After that come back to script and "
+pausePressKey
+echo ""
 ./94-setpath.sh
